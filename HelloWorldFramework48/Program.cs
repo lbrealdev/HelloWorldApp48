dfc46a799438f2b1e16c88fab89a53fb88a3b6c3
lbrealdev <lbrealdeveloper@gmail.com>
2024-12-08 23:13:51 +0100

    feat: add HelloWorld .NET framework project

---
+++ HelloWorldFramework48/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldFramework48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String hostname = Environment.MachineName;
+
+            Console.WriteLine($"Hello World {hostname}!");
+
+            Console.ReadLine();
+        }
+    }
+}
